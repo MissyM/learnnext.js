943522b04e21866350bda90581f7fe337e337f04
Mile <mcabrerapatino@gmail.com>
2018-04-17 16:14:28 -0500

    Using shared component finished

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-//import Header from '../comps/Header'
 import Layout from '../comps/Layout.js'
 export default () => (
   <div>
     <Layout>
-      {/*<Header/>*/}
        <p>This is the about page.</p>
     </Layout>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
-//import Link from 'next/link'
-//import Header from '../comps/Header'
 import Layout from '../comps/Layout.js'
-
-
-// const Index = () => (
-//   <div>
-//     <Link href="/about">
-//       <button style={{ fontSize: 20 }}>Propietarios</button>
-//     </Link>
-//     <p>Hello Next.js</p>
-//   </div>
-// )
 
 export default () => (
   <div>
     <Layout>
-    {/*<Header/>*/}
     <p>Hola esta es la página principal</p>
     </Layout>
   </div>
