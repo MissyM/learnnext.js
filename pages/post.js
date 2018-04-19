2b103006b820782f937146ee05f0d4e15b5edd37
Mile <mcabrerapatino@gmail.com>
2018-04-19 15:44:00 -0500

    Fetch Data in Client Side

--- pages/post.js
+++ pages/post.js
@@ -2,15 +2,36 @@ import Layout from '../components/Layout.js'
 import fetch from 'isomorphic-unfetch'
 
 
-const Content = (props) => (
-  <div>
-    <h1>{props.url.query.title}</h1>
-    <p>Este es el contenido del post.</p>
-  </div>
+const Post =  (props) => (
+  <Layout>
+       <h1>{props.show.name}</h1>
+       
+  </Layout>
 )
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
 
-export default (props) => (
-  <Layout> 
-    <Content url = {props.url}/>
-  </Layout> 
-)
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
+
+
+// const Content = (props) => (
+//   <div>
+//     <h1>{props.show.name}</h1>
+//        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+//        <img src={props.show.image.medium}/>
+//   </div>
+// )
+
+
+// export default (props) => (
+//   <Layout> 
+//     <Content url = {props.url}/>
+//   </Layout> 
+// )
