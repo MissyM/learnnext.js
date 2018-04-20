940d07843bd356e4f66aec6a4acced2f2713b243
Mile <mcabrerapatino@gmail.com>
2018-04-20 16:03:40 -0500

    react-markdown

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,18 @@
 import Layout from '../components/Layout.js'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-function getPosts () {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js'},
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
-  ]
-}
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      h1, a {
-        font-family: "Arial";
-      }
-
-      ul {
-        padding: 0;
-      }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-)
-
-export default () => (
+const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Girls TV Shows</h1>
     <ul>
-      {getPosts().map((post) => (
-        <PostLink key={post.id} post={post}/>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
       ))}
     </ul>
     <style jsx>{`
@@ -74,78 +41,34 @@ export default () => (
   </Layout>
 )
 
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=girls')
+  const data = await res.json()
 
-// import Layout from '../components/Layout.js'
-// import Link from 'next/link'
-// import fetch from 'isomorphic-unfetch'
+  console.log(`Show data fetched. Count: ${data.length}`)
 
+  return {
+    shows: data
+  }
+}
 
-// const Index = (props) => (
-//   <Layout>
-//     <h1>Girls TV Shows</h1>
-//     <ul>
-//       {props.shows.map(({show}) => (
-//         <li key={show.id}>
-//           <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-//             <a>{show.name}</a>
-//           </Link>
-//         </li>
-//       ))}
-//     </ul>
-//     {/* <style jsx>{`
-//       h1, a {
-//         font-family: "Arial";
-//       }
+export default Index
 
-//       ul {
-//         padding: 0;
-//       }
-
-//       li {
-//         list-style: none;
-//         margin: 5px 0;
-//       }
-
-//       a {
-//         text-decoration: none;
-//         color: blue;
-//       }
-
-//       a:hover {
-//         opacity: 0.6;
-//       }
-//     `}</style> */}
-//   </Layout>
+// const PostLink = (props) => (
+//   <li>
+//      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//       <a>{props.title}</a>
+//      </Link>
+//   </li>
 // )
 
-// Index.getInitialProps = async function() {
-//   const res = await fetch('https://api.tvmaze.com/search/shows?q=girls')
-//   const data = await res.json()
-
-//   console.log(`Show data fetched. Count: ${data.length}`)
-
-//   return {
-//     shows: data
-//   }
-// }
-
-// export default Index
-
-// // const PostLink = (props) => (
-// //   <li>
-// //      <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-// //       <a>{props.title}</a>
-// //      </Link>
-// //   </li>
-// // )
-
-// // export default () => (
-// //   <Layout>
-// //     <h1>My Blog</h1>
-// //     <ul>
-// //       <PostLink id="Propietarios" title="Propietarios"/>
-// //       <PostLink id="Negocios" title="Negocios"/>
-// //       <PostLink id="Vendedores" title="Vendedores"/>
-// //     </ul>
-// //   </Layout>
-// // )
+// export default () => (
+//   <Layout>
+//     <h1>My Blog</h1>
+//     <ul>
+//       <PostLink id="Propietarios" title="Propietarios"/>
+//       <PostLink id="Negocios" title="Negocios"/>
+//       <PostLink id="Vendedores" title="Vendedores"/>
+//     </ul>
+//   </Layout>
+// )
--- pages/post.js
+++ pages/post.js
@@ -1,24 +1,61 @@
 import Layout from '../components/Layout.js'
-import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
+//import fetch from 'isomorphic-unfetch'
 
-
-const Post =  (props) => (
+export default (props) => (
   <Layout>
-       <h1>{props.show.name}</h1>
+   <h1>{props.url.query.title}</h1>
+   <div className="markdown">
+     <Markdown source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
 
+### This is a title
+
+And here's the content.
+     `}/>
+   </div>
+   <style jsx global>{`
+     .markdown {
+       font-family: 'Arial';
+     }
+
+     .markdown a {
+       text-decoration: none;
+       color: blue;
+     }
+
+     .markdown a:hover {
+       opacity: 0.6;
+     }
+
+     .markdown h3 {
+       margin: 0;
+       padding: 0;
+       text-transform: uppercase;e
+     }
+  `}</style>
   </Layout>
 )
-Post.getInitialProps = async function (context) {
-  const { id } = context.query
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
-  const show = await res.json()
 
-  console.log(`Fetched show: ${show.name}`)
+// const Post =  (props) => (
+//   <Layout>
+//        <h1>{props.show.name}</h1>
 
-  return { show }
-}
+//   </Layout>
+// )
+// Post.getInitialProps = async function (context) {
+//   const { id } = context.query
+//   const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+//   const show = await res.json()
 
-export default Post
+//   console.log(`Fetched show: ${show.name}`)
+
+//   return { show }
+// }
+
+// export default Post
 
 
 // const Content = (props) => (
